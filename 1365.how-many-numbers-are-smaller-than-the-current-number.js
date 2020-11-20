9841ff9d3a201ff8ecc731a6741d68a247c3471f
Jelly38214 <goolezheng@gmail.com>
2020-11-20 21:47:59 +0800

    feat(1365): Count how many numbers are smaller than the current number using two for loop.

---
+++ 1365.how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1365 lang=javascript
+ *
+ * [1365] How Many Numbers Are Smaller Than the Current Number
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function (nums) {
+  const res = [];
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] < nums[i]) { count++ }
+    }
+    res.push(count);
+    count = 0;
+  }
+
+  return res;
+};
+// @lc code=end
+
